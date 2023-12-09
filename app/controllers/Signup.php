15a332b3a060ac016613cade1a6e8268a6227bf9
Anuththara93 <2021cs009@stu.ucsc.cmb.ac.lk>
2023-12-09 17:26:28 +0530

    Add file structure

---
+++ app/controllers/Signup.php
@@ -0,0 +1,17 @@
+<?php
+
+//SignUp class
+
+class Signup extends Controller{
+    
+    public function index()
+    {
+        // $db = new Database();
+        // $db->create_tables();
+        
+        show($_POST);
+        $data['title'] = "Signup";
+
+        $this->view('signup', $data);
+    }
+}

1f421210a39cbd239a4055bf4de33237d678056a
Anuththara93 <2021cs009@stu.ucsc.cmb.ac.lk>
2023-12-10 10:16:22 +0530

    User form validation

--- app/models/User.php
+++ app/models/User.php
@@ -2,14 +2,39 @@
 
 //users model
 
-class Userr{
+class User{
     
-    protected $errors = [];
+    public $errors = [];
     protected $table = "users";
 
-    public function validate()
+    public function validate($data)
     {
         $this->errors = [];
+
+        if(empty($data['firstname'])){
+            $this->errors['firstname'] = "First name is required";
+        }
+
+        if(empty($data['lastname'])){
+            $this->errors['lastname'] = "Last name is required";
+        }
+
+        if(empty($data['email'])){
+            $this->errors['email'] = "Email is required";
+        }
+
+        if(empty($data['password'])){
+            $this->errors['password'] = "A password is required";
+        }
+
+        if($data['password'] !== $data['retype_password']){
+            $this->errors['password'] = "Passwords do not match";
+        }
+
+        if(empty($data['terms'])){
+            $this->errors['terms'] = "Please accept the terms and conditions";
+        }
+
         
         if(empty($this->errors)){
             return true;
